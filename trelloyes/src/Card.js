ea700ded96c6aa759f2bc00f47b1edf8fbae19dc
a_frear <amy.frear@gmail.com>
2020-11-15 14:03:26 -0500

    Initial commit

---
+++ trelloyes/src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Card.css'; 
+
+function Card(props) {
+    return <div class="Card">
+        <button type="button">delete</button>
+        <h3>{props.title}</h3>
+        <p>{props.content}</p>
+    </div>
+  }
+  
+  export default Card;
+
+  //The Card component accepts 2 props: title and content.
+//title is a string of the card's title.
+//content is a string of the card's content.
+//These props will be passed in for each Card from the List component.
