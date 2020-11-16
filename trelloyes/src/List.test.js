9ae30308c833dc46066479211bf0f74a032b5d67
a_frear <amy.frear@gmail.com>
2020-11-16 10:27:52 -0500

    Update List.test.js

--- trelloyes/src/List.test.js
+++ trelloyes/src/List.test.js
@@ -6,17 +6,17 @@ import List from './List.js';
 describe('Card component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<List />, div);
+    ReactDOM.render(<List cards={[]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('renders the UI as expected', () => {
-    const c = [
+    const card = [
         { id: 'a', title: 'Amy', content: 'Philly' },
         { id: 'b', title: 'Tini', content: 'Tahoe' }
     ]
     const tree = renderer
-    .create(<List key={1} header={'First list'} cards={c} />)
+    .create(<List key={1} header={'First list'} cards={card} />)
     .toJSON();
 expect(tree).toMatchSnapshot();
 })
